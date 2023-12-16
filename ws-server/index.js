b04b422187bafeffce4cf2b2bac3f680c2304563
Meet <meetaparekh2003@gmail.com>
2023-12-16 18:20:00 +0530

    Getting message an input

--- ws-server/index.js
+++ ws-server/index.js
@@ -15,12 +15,18 @@ app.get('/', (req, res) => {
   });
   
 
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+// //     socket.on('disconnect', () => {
+// //       console.log('user disconnected');
+// //    });
+// });
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
-//     socket.on('disconnect', () => {
-//       console.log('user disconnected');
-//    });
-});
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+  });
   
 
 server.listen(3000, () => {
